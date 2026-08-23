a1497651e5e02fa69541eafb2ae2b96ee283011d
Bronas5555 <phil42092009@gmail.com>
2026-08-23 19:37:31 +0200

    Remove Collission at MPG Hypogean Gaol

--- Randomizer/FogRandomizer.cs
+++ Randomizer/FogRandomizer.cs
@@ -29,7 +29,7 @@ public class FogRandomizer
     {
          Generate(null);
          
-         //FogGates.GateAfterIosefkasClinicToClinic.Link(FogGates.RomArenaEntrance);
+         //FogGates.GateAfterIosefkasClinicToClinic.Link(FogGates.MPGHypogeanGoalPaarl);
          
          return _unusedGates.Count;
     }
